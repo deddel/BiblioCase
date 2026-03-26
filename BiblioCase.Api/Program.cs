85bdc46671c2bc233ba4b5f34d7d14d77452e530
Dennis <83078233+deddel@users.noreply.github.com>
2026-03-26 19:28:23 +0100

    Initial Migration

--- BiblioCase.Api/Program.cs
+++ BiblioCase.Api/Program.cs
@@ -1,9 +1,15 @@
+using BiblioCase.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
--- BiblioCase.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BiblioCase.Infrastructure;
-
-public class Class1
-{
-
-}
